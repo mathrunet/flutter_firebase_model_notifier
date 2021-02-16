f3655d74cecb0f61b9d680cd698846c579a4691b
mathru <user@mathru.net>
2021-02-16 23:30:15 +0900

    パラメーターでクエリーを設定できるように修正

--- lib/firestore/firestore_collection_model.dart
+++ lib/firestore/firestore_collection_model.dart
@@ -2,18 +2,90 @@ part of firestore_model_notifier;
 
 abstract class FirestoreCollectionModel<T extends FirestoreDocumentModel>
     extends ListModel<T> implements StoredModel<List<T>> {
-  FirestoreCollectionModel(this.path, [List<T>? value])
+  FirestoreCollectionModel(String path, [List<T>? value])
       : assert(!(path.splitLength() <= 0 || path.splitLength() % 2 != 1),
             "The path hierarchy must be an odd number."),
+        path = _getPath(path),
+        paramaters = _getParamaters(path),
         super(value ?? []);
+
+  static String _getPath(String path) {
+    if (path.contains("?")) {
+      return path.split("?").first;
+    }
+    return path;
+  }
+
+  static Map<String, String> _getParamaters(String path) {
+    if (path.contains("?")) {
+      return Uri.parse(path).queryParameters;
+    }
+    return const {};
+  }
 
   @protected
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
 
   final String path;
+  final Map<String, String> paramaters;
 
   @protected
-  Query query(Query query) => query;
+  @mustCallSuper
+  Query query(Query query) {
+    if (paramaters.isNotEmpty) {
+      if (!paramaters.containsKey("orderBy")) {
+        return query;
+      }
+      if (paramaters.containsKey("equalTo")) {
+        query = query.where(paramaters["orderBy"],
+            isEqualTo: paramaters["equalTo"]);
+      }
+      if (paramaters.containsKey("notEqualTo")) {
+        query = query.where(paramaters["orderBy"],
+            isNotEqualTo: paramaters["noteEqualTo"]);
+      }
+      if (paramaters.containsKey("startAt")) {
+        query = query.where(paramaters["orderBy"],
+            isGreaterThanOrEqualTo: double.parse(paramaters["startAt"] ?? "0"));
+      }
+      if (paramaters.containsKey("endAt")) {
+        query = query.where(paramaters["orderBy"],
+            isLessThanOrEqualTo: double.parse(paramaters["endAt"] ?? "0"));
+      }
+      if (paramaters.containsKey("contains")) {
+        query = query.where(paramaters["orderBy"],
+            arrayContains: paramaters["arrayIn"]);
+      }
+      if (paramaters.containsKey("containsAny")) {
+        query = query.where(paramaters["orderBy"],
+            arrayContainsAny: paramaters["containsAny"]?.split(","));
+      }
+      if (paramaters.containsKey("whereIn")) {
+        query = query.where(paramaters["orderBy"],
+            whereIn: paramaters["whereIn"]?.split(","));
+      }
+      if (paramaters.containsKey("whereNotIn")) {
+        query = query.where(paramaters["orderBy"],
+            whereNotIn: paramaters["whereNotIn"]?.split(","));
+      }
+      if (paramaters.containsKey("orderBy")) {
+        query = query.orderBy(paramaters["orderBy"]);
+      }
+      if (paramaters.containsKey("limitToFirst")) {
+        query = query.limit(int.parse(paramaters["limitToFirst"] ?? "0"));
+      }
+      if (paramaters.containsKey("limitToLast")) {
+        query = query.limitToLast(int.parse(paramaters["limitToLast"] ?? "0"));
+      }
+      if (paramaters.containsKey("orderByDesc")) {
+        query = query.orderBy(paramaters["orderByDesc"]);
+      }
+      if (paramaters.containsKey("orderByDesc")) {
+        query = query.where(paramaters["orderByDesc"]);
+      }
+    }
+    return query;
+  }
 
   @protected
   @mustCallSuper
@@ -46,6 +118,9 @@ abstract class FirestoreCollectionModel<T extends FirestoreDocumentModel>
   bool get notifyOnChangeList => false;
 
   Query get reference {
+    if (path.contains("?")) {
+      return query(firestore.collection(path.split("?").first));
+    }
     return query(firestore.collection(path));
   }
 

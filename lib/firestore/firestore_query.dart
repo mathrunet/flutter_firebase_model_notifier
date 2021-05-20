fb3104bbb7925f25605e410465b413252d844a66
mathru <user@mathru.net>
2021-05-20 18:23:09 +0900

    CollectionQueryに対応

--- lib/firestore/firestore_query.dart
+++ lib/firestore/firestore_query.dart
@@ -1,85 +1,36 @@
 part of firebase_model_notifier;
 
-class FirestoreQuery {
+class FirestoreQuery extends CollectionQuery {
   const FirestoreQuery(
-    this.path, {
-    this.key,
-    this.isEqualTo,
-    this.isNotEqualTo,
-    // this.isLessThan,
-    this.isLessThanOrEqualTo,
-    // this.isGreaterThan,
-    this.isGreaterThanOrEqualTo,
-    this.arrayContains,
-    this.arrayContainsAny,
-    this.whereIn,
-    this.whereNotIn,
-    this.order = FirestoreQueryOrder.asc,
-    this.limit,
-    this.orderBy,
-  });
-  final String path;
-  final String? key;
-  final String? orderBy;
-  final dynamic isEqualTo;
-  final dynamic isNotEqualTo;
-  // final dynamic isLessThan;
-  final dynamic isLessThanOrEqualTo;
-  // final dynamic isGreaterThan;
-  final dynamic isGreaterThanOrEqualTo;
-  final dynamic arrayContains;
-  final List<dynamic>? arrayContainsAny;
-  final List<dynamic>? whereIn;
-  final List<dynamic>? whereNotIn;
-  // final bool? isNull;
-  final FirestoreQueryOrder order;
-  final int? limit;
-
-  String _limit(String path) {
-    if (limit == null) {
-      return path;
-    }
-    return "$path&limitToFirst=$limit";
-  }
-
-  String _order(String path) {
-    if (orderBy.isEmpty) {
-      return path;
-    }
-    if (order == FirestoreQueryOrder.asc) {
-      return "$path&orderByAsc=$orderBy";
-    } else {
-      return "$path&orderByDesc=$orderBy";
-    }
-  }
-
-  String get value {
-    if (key.isEmpty) {
-      return path;
-    }
-    final tmp = "$path?key=$key";
-    if (isEqualTo != null) {
-      return _limit(_order("$tmp&equalTo=$isEqualTo"));
-    } else if (isNotEqualTo != null) {
-      return _limit(_order("$tmp&notEqualTo=$isNotEqualTo"));
-    } else if (isLessThanOrEqualTo != null) {
-      return _limit(_order("$tmp&endAt=$value"));
-    } else if (isGreaterThanOrEqualTo != null) {
-      return _limit(_order("$tmp&startAt=$value"));
-    } else if (arrayContains != null) {
-      return _limit(_order("$tmp&contains=$arrayContains"));
-    } else if (arrayContainsAny != null) {
-      return _limit(_order(
-          "$tmp&containsAny=${arrayContainsAny!.map((e) => e.toString()).join(",")}"));
-    } else if (whereIn != null) {
-      return _limit(_order(
-          "$tmp&whereIn=${whereIn!.map((e) => e.toString()).join(",")}"));
-    } else if (whereNotIn != null) {
-      return _limit(_order(
-          "$tmp&whereNotIn=${whereNotIn!.map((e) => e.toString()).join(",")}"));
-    }
-    return tmp;
-  }
+    String path, {
+    String? key,
+    String? orderBy,
+    dynamic isEqualTo,
+    dynamic isNotEqualTo,
+    // dynamic isLessThan;
+    dynamic isLessThanOrEqualTo,
+    // dynamic isGreaterThan;
+    dynamic isGreaterThanOrEqualTo,
+    dynamic arrayContains,
+    List<dynamic>? arrayContainsAny,
+    List<dynamic>? whereIn,
+    List<dynamic>? whereNotIn,
+    // bool? isNull;
+    CollectionQueryOrder order = CollectionQueryOrder.asc,
+    int? limit,
+  }) : super(
+          path,
+          key: key,
+          orderBy: orderBy,
+          isEqualTo: isEqualTo,
+          isNotEqualTo: isNotEqualTo,
+          isLessThanOrEqualTo: isLessThanOrEqualTo,
+          isGreaterThanOrEqualTo: isGreaterThanOrEqualTo,
+          arrayContains: arrayContains,
+          arrayContainsAny: arrayContainsAny,
+          whereIn: whereIn,
+          whereNotIn: whereNotIn,
+          order: order,
+          limit: limit,
+        );
 }
-
-enum FirestoreQueryOrder { asc, desc }
